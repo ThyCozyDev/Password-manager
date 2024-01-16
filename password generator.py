08be17ecfcc75a716b409b75c108a09adf145e37
thycozydev <thycozydev@gmail.com>
2024-01-16 17:03:29 +0200

    Added a delete button and and improved the error handling

--- password generator.py
+++ password generator.py
@@ -1,35 +1,13 @@
-import random
-import string
 import PySimpleGUI as sg
 import pyperclip
-def generate_password(length):
-    """
-    Generate a random password of the specified length.
-    """
-    uppercase_letters = string.ascii_uppercase
-    lowercase_letters = string.ascii_lowercase
-    digits = string.digits
-    special_chars = string.punctuation
+from functions import functions
 
-    all_chars = uppercase_letters + lowercase_letters + digits + special_chars
 
-    password = ''.join(random.choice(all_chars) for _ in range(length))
-
-    return password
-
-def load_passwords_from_file(filename="passwords.txt"):
-    """
-    Load passwords from a file.
-    """
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            return [line.strip() for line in file.readlines()]
-    except FileNotFoundError:
-        return []
+function_instance = functions()
 
 PASSWORDS_FILE = "passwords.txt"
 
-passwords = load_passwords_from_file(PASSWORDS_FILE)
+passwords = functions.load_passwords_from_file(PASSWORDS_FILE)
 
 layout = [
     [sg.Text("Password Length:"), sg.InputText(key="length")],
@@ -37,7 +15,8 @@ layout = [
     [sg.Button("Generate", key="button")],
     [sg.InputText(readonly=True, key="Password")],
     [sg.Text(text="Passwords")],
-    [sg.Listbox(values=passwords, size=(50, 10), key="PASSWORDLIST")],
+    [sg.Listbox(values=passwords, size=(50, 10), key="PASSWORDLIST"),
+      sg.Button(button_text="Delete", key="DeleteButton")],
     [sg.Button(button_text="Copy password", key="copy_button")],
 ]
 
@@ -48,29 +27,41 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
-
+    
     if event == "button":
-        length = values["length"]
+        password_length = values["length"]
         website = values["Website"]
-        length = int(length)
         
-        # Check if length is a positive integer
-        if length <= 0:
-            sg.popup_error("Please enter a positive integer for password length.")
+        # Check if password_length is a valid number
+        if not password_length.isdigit():
+            sg.popup_error("Please enter a valid number for password length.")
             continue
         
+        password_length = int(password_length)
+        
+        # Check if password_length is a positive integer
+        if password_length <= 0:
+            sg.popup_error("Please enter a positive integer for password length.")
+            continue
+    
         # password generation
-        generated_password = generate_password(length)
-        with open(PASSWORDS_FILE, "a", encoding="utf-8") as file:
-            file.writelines(f"Website: {website}, Password: {generated_password}\n")
-        window["Password"].update(f"Your password is: {generated_password}")
+        GENERATED_PASSWORD = function_instance.generate_password(password_length=password_length)
+        with open(PASSWORDS_FILE, "a", encoding="utf-8") as password_file:
+            password_file.writelines(f"Website: {website}, Password: {GENERATED_PASSWORD}\n")
+        window["Password"].update(f"Your password is: {GENERATED_PASSWORD}")
         # Update the passwords list and refresh the Listbox
-        passwords.append(f"Website: {website}, Password: {generated_password}")
+        passwords.append(f"Website: {website}, Password: {GENERATED_PASSWORD}")
         window["PASSWORDLIST"].update(values=passwords)
     
     if event == "copy_button":
         selected_password = values["PASSWORDLIST"][0]
         password = selected_password.split(": ")[-1]
         pyperclip.copy(password)
+
+    if event == "DeleteButton":
+        selected_password = values["PASSWORDLIST"][0]
+        function_instance.delete_password(selected_password=selected_password, passwords=passwords)
+        # Update the Listbox with the new passwords list
+        window["PASSWORDLIST"].update(values=passwords)
 
 window.close()
