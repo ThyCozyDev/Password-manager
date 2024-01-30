4693a05759ddfa6ef0e783acd0453416adde3e5e
ThyCozyDev <thycozydev@gmail.com>
2024-01-30 13:25:34 +0200

    changed the saving of the password to an aes encryption file

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 import string
 import random
 import json
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+import os
 
 
 class PasswordFunctions:
@@ -30,6 +34,50 @@ class PasswordFunctions:
         except FileNotFoundError:
             return []
         
+    def encrypt_passwords(self, passwords, key):
+        cipher = AES.new(key, AES.MODE_CBC)
+        ct_bytes = cipher.encrypt(pad(passwords.encode('utf-8'), AES.block_size))
+        iv = cipher.iv
+        return iv + ct_bytes
+
+    def decrypt_passwords(self, encrypted_data, key):
+        iv = encrypted_data[:16]
+        ct = encrypted_data[16:]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        pt = unpad(cipher.decrypt(ct), AES.block_size)
+        return pt.decode('utf-8')
+
+    def save_passwords_to_file(self, passwords, filename="passwords.aes"):
+        key = self.load_encryption_key()
+        # Join the passwords list into a single string separated by newlines
+        passwords_str = "\n".join(passwords)
+        encrypted_data = self.encrypt_passwords(passwords_str, key)
+        with open(filename, "wb") as password_file:
+            password_file.write(encrypted_data)
+
+    def load_passwords_from_file(self, filename="passwords.aes"):
+        key = self.load_encryption_key()
+        try:
+            with open(filename, "rb") as password_file:
+                encrypted_data = password_file.read()
+            passwords_str = self.decrypt_passwords(encrypted_data, key)
+            return passwords_str.split("\n")
+        except FileNotFoundError:
+            return []
+
+    def load_encryption_key(self, key_file=".encryption_key"):
+        try:
+            with open(key_file, "rb") as key_file:
+                key = key_file.read()
+        except FileNotFoundError:
+            key = get_random_bytes(16)
+            with open(key_file, "wb") as key_file:
+                key_file.write(key)
+                # Hide the key file if on Windows
+                if os.name == 'nt':
+                    os.system(f"attrib +h {key_file.name}")
+        return key
+    
     def delete_password(self, selected_password, passwords):
         """
         Delete a selected password from the list and update the passwords file.
@@ -37,12 +85,8 @@ class PasswordFunctions:
         # Remove the selected password from the list
         if selected_password in passwords:
             passwords.remove(selected_password)
-
-        # Update the passwords file
-        with open("passwords.txt", "w", encoding="utf-8") as password_file:
-            for password in passwords:
-                password_file.write(password + "\n")      
-                
+            # Update the passwords file
+            self.save_passwords_to_file(passwords, "passwords.aes")
     CONFIG_FILE = "config.json"
 
     def save_theme(self, theme_name):
